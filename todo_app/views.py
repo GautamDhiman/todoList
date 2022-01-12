4a5f20c781e3e05d016926f47c88140555925a4b
GautamDhiman <gautamCodesHere@gmail.com>
2022-01-12 12:45:22 +0530

    added CRUD placeholder views

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def seeAllTasks(request):
+    
+    # GET
+    return HttpResponse('All tasks')
+
+def addTasks(request):
+    
+    #Post
+    return HttpResponse("add tasks")
+
+def getSingleTask(request, id):
+
+
+    #Get
+    return HttpResponse("get single task")
+
+def updateTask(request, id):
+
+
+    #Get
+    return HttpResponse("Update task")
+
+def deleteTask(request, id):
+
+    #Get
+    return HttpResponse("Delete task {0}".format(id))
