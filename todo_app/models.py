f30b9aaf94db304918a068d58cc1cefd4c22c85f
GautamDhiman <gautamCodesHere@gmail.com>
2022-01-12 13:09:31 +0530

    registered model named Task

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    
+    title = models.CharField(max_length=10)
+    desc = models.CharField(max_length=50)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
